725ec0f06c275e91409c70e64ab4db3140ddeb00
David Lara <dlara7913@gmail.com>
2023-08-05 21:18:57 -0700

    -Added from scratch gaussian blur. -Added comments on some code. -Fixed function names.

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -59,7 +59,6 @@ def compute_convolution(PixelMatrix, XDirection, YDirection):
 def greyscale_image(image):
     img = Image.open(image)
     pixels = img.load()
-    img.show()
 
     height, width = img.size
     for i in range(0, height):
@@ -87,4 +86,54 @@ def image_inversion(image):
     return img
 
 
+def gaussian_blur(image):
+    """Apply a gaussian blur to an image with the sigma set to 1, and the radius of the kernel set to 1"""
+    img = Image.open(image)
+    imgNew = Image.open(image)
+    pixels = img.load()
+    pixelsNew = imgNew.load()
 
+    height, width = img.size
+    radius = 1
+    sigma = 1
+    kernel_width = (2 * radius) + 1
+    kernel_matrix = numpy.zeros([kernel_width, kernel_width], dtype=float)
+    kernel_sum = 0
+
+    compute_weight_matrix(kernel_matrix, kernel_sum, radius, sigma, kernel_width)
+
+    for x in range(radius, height - radius):
+        for y in range(radius, width - radius):
+            red = 0
+            green = 0
+            blue = 0
+
+            for kernel_x in range(-radius, radius + 1):
+                for kernel_y in range(-radius, radius + 1):
+                    kernel_value = kernel_matrix[kernel_x + radius][kernel_y + radius]
+                    red += img.getpixel((x - kernel_x, y - kernel_y))[0] * kernel_value
+                    green += img.getpixel((x - kernel_x, y - kernel_y))[1] * kernel_value
+                    blue += img.getpixel((x - kernel_x, y - kernel_y))[2] * kernel_value
+            pixelsNew[x, y] = (int(red), int(green), int(blue))
+
+    return imgNew
+
+
+def compute_weight_matrix(k_matrix, sum, radius, sigma, width):
+    """Precomputes the weight matrix used for applying the gaussian blur to an image"""
+    gauss_denominator = 1 / (2 * math.pi * sigma ** 2)
+    for x in range(-radius, radius + 1):
+        for y in range(-radius, radius + 1):
+            gauss_exponent = -((x ** 2) + (y ** 2)) / (2 * (sigma ** 2))
+            gauss_value = gauss_denominator * (math.e ** gauss_exponent)
+            k_matrix[x][y] = gauss_value
+            sum += gauss_value
+
+    normalize_matrix(k_matrix, sum, width)
+
+
+def normalize_matrix(k_matrix, sum, k_wdith):
+    """Normalize the weights in the matrix so that the entire matrix can add up to 1"""
+    for x in range(k_wdith):
+        for y in range(k_wdith):
+            k_matrix[x][y] = k_matrix[x][y] / sum
